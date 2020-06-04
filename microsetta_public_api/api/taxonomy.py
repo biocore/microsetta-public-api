ba6577a04c96a2366c356e8607e372ea6a3e5b00
George <garmstro@eng.ucsd.edu>
2020-06-03 18:00:47 -0700

    TST: add tests for taxonomy implementation with some reworking based on tests

--- microsetta_public_api/api/taxonomy.py
+++ microsetta_public_api/api/taxonomy.py
@@ -1,3 +1,4 @@
+import json
 from microsetta_public_api.repo._taxonomy_repo import TaxonomyRepo
 from microsetta_public_api.utils import jsonify
 from microsetta_public_api.models._taxonomy import Taxonomy
@@ -7,13 +8,13 @@ from microsetta_public_api.utils._utils import (validate_resource,
 
 
 def summarize_group(body, resource):
-    sample_ids = body['sample_ids']
+    sample_ids = json.loads(body)['sample_ids']
     return _summarize_group(sample_ids, resource)
 
 
 def _summarize_group(sample_ids, table_name):
     taxonomy_repo = TaxonomyRepo()
-    available_resources = taxonomy_repo.available_resources()
+    available_resources = taxonomy_repo.resources()
 
     type_ = 'resource'
     missing_resource = validate_resource(available_resources, table_name,
@@ -28,21 +29,21 @@ def _summarize_group(sample_ids, table_name):
     if missing_ids_msg:
         return missing_ids_msg
 
-    table = taxonomy_repo.table(sample_ids, table_name)
+    table = taxonomy_repo.table(table_name)
     features = taxonomy_repo.feature_data_taxonomy(table_name)
-    variances = taxonomy_repo.variances(sample_ids, table_name)
+    variances = taxonomy_repo.variances(table_name)
 
     taxonomy_ = Taxonomy(table, features, variances)
-    taxonomy_data = taxonomy_.get_group(sample_ids, '')
+    taxonomy_data = taxonomy_.get_group(sample_ids, '').to_dict()
     del taxonomy_data['name']
     del taxonomy_data['feature_ranks']
     response = jsonify(taxonomy_data)
     return response, 200
 
 
-def resources_available():
+def resources():
     taxonomy_repo = TaxonomyRepo()
     ret_val = {
-        'resources': taxonomy_repo.available_resources(),
+        'resources': taxonomy_repo.resources(),
     }
     return jsonify(ret_val), 200
