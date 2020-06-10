aa672c996b87c931195afaec8e96b7c7dcf380a0
gwarmstrong <garmstrong@colgate.edu>
2020-06-10 16:12:28 -0700

    more details in metadata repo

--- microsetta_public_api/repo/_metadata_repo.py
+++ microsetta_public_api/repo/_metadata_repo.py
@@ -4,14 +4,21 @@ from microsetta_public_api.resources import resources
 class MetadataRepo:
 
     def __init__(self):
-        raise NotImplementedError()
+        self._metadata = resources.get('metadata', pd.DataFrame())
 
-    @@property
+    @property
+    def metadata(self):
+        return self._metadata
+
+    @property
     def categories(self):
-        raise NotImplementedError()
+        return list(self._metadata.columns)
 
     def category_values(self, category):
-        raise NotImplementedError()
+        if category not in self._metadata.columns:
+            raise ValueError(f'No category with name `{category}`')
+        else:
+            return list(self._metadata[category].unique())
 
     def sample_id_matches(self, query):
         raise NotImplementedError()
