ca14254d34d50988a7cf5479c3438dc58353f61c
gwarmstrong <garmstrong@colgate.edu>
2020-06-10 17:52:57 -0700

    TST add test cases for errors in repo search

--- microsetta_public_api/repo/tests/test_metadata_repo.py
+++ microsetta_public_api/repo/tests/test_metadata_repo.py
@@ -18,6 +18,7 @@ class TestMetadataRepo(TempfileTestCase, ConfigTestCase):
         self.test_metadata = pd.DataFrame({
                 'age_cat': ['30s', '40s', '50s', '30s'],
                 'num_cat': [7.24, 7.24, 8.25, 7.24],
+                'other': [1, 2, 3, 4],
             }, index=pd.Series(['a', 'b', 'c', 'd'], name='#SampleID')
         )
         Metadata(self.test_metadata).save(self.metadata_filename)
@@ -30,7 +31,7 @@ class TestMetadataRepo(TempfileTestCase, ConfigTestCase):
         ConfigTestCase.tearDown(self)
 
     def test_categories(self):
-        exp = ['age_cat', 'num_cat']
+        exp = ['age_cat', 'num_cat', 'other']
         obs = self.repo.categories
         self.assertCountEqual(exp, obs)
 
@@ -64,6 +65,37 @@ class TestMetadataRepo(TempfileTestCase, ConfigTestCase):
         obs = self.repo.sample_id_matches(query)
         self.assertCountEqual(exp, obs)
 
+    def test_category_sample_id_matches_query_nested(self):
+        exp = ['a', 'c', 'd']
+        query = {
+            "condition": "OR",
+            "rules": [
+                {
+                    "condition": "AND",
+                    "rules": [
+                        {
+                            "id": "age_cat",
+                            "operator": "equal",
+                            "value": "30s",
+                        },
+                        {
+                            "id": "num_cat",
+                            "operator": "equal",
+                            "value": 7.24,
+                        }
+                    ]
+
+                },
+                {
+                    "id": "other",
+                    "operator": "greater_or_equal",
+                    "value": 3,
+                },
+            ],
+        }
+        obs = self.repo.sample_id_matches(query)
+        self.assertCountEqual(exp, obs)
+
     def test_category_sample_id_matches_query_single_category(self):
         exp = ['c']
         query = {
@@ -88,3 +120,61 @@ class TestMetadataRepo(TempfileTestCase, ConfigTestCase):
         }
         obs = self.repo.sample_id_matches(query)
         self.assertCountEqual(exp, obs)
+
+    def test_category_sample_id_ill_formed_query_no_condition(self):
+        query = {
+            "rules": [
+                {
+                    "id": "age_cat",
+                    "operator": "equal",
+                    "value": "50s",
+                }
+            ]
+        }
+        with self.assertRaisesRegex(ValueError, r'does not appear to be a '
+                                                r'rule or a group'):
+            self.repo.sample_id_matches(query)
+
+    def test_category_sample_id_ill_formed_query_bad_rule(self):
+        query = {
+            "condition": "AND",
+            "rules": [
+                {
+                    "id": "age_cat",
+                    "value": "50s",
+                }
+            ]
+        }
+        with self.assertRaisesRegex(ValueError, r'does not appear to be a '
+                                                r'rule or a group'):
+            self.repo.sample_id_matches(query)
+
+    def test_category_sample_id_ill_formed_query_unsupported_condition(self):
+        query = {
+            "condition": "XOR",
+            "rules": [
+                {
+                    "id": "age_cat",
+                    "value": "50s",
+                    "operator": "equal"
+                }
+            ]
+        }
+        with self.assertRaisesRegex(ValueError, r'Only conditions in (.*) '
+                                                r'are supported. Got '):
+            self.repo.sample_id_matches(query)
+
+    def test_category_sample_id_ill_formed_query_unsupported_operator(self):
+        query = {
+            "condition": "AND",
+            "rules": [
+                {
+                    "id": "age_cat",
+                    "value": "50s",
+                    "operator": "something_weird"
+                }
+            ]
+        }
+        with self.assertRaisesRegex(ValueError, r'Only operators in (.*) '
+                                                r'are supported. Got '):
+            self.repo.sample_id_matches(query)
