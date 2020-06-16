c6798ff6b6c1a6cc45abb6e006d5b5a2cdabb59c
gwarmstrong <garmstrong@colgate.edu>
2020-06-16 10:53:53 -0700

    MAINT refactor the repeated code out

--- microsetta_public_api/api/metadata.py
+++ microsetta_public_api/api/metadata.py
@@ -22,49 +22,37 @@ def filter_sample_ids(taxonomy=None, alpha_metric=None, **kwargs):
         return is_invalid
     matching_ids = repo.sample_id_matches(query)
 
-    response = dict()
-    error_response = None
-    error_code = None
+    matching_ids, error_code, error_response = _filter_matching_ids(
+        matching_ids, TaxonomyRepo, 'resources', taxonomy, 'resource',
+    )
 
-    repo = TaxonomyRepo
-    method = 'resources'
-    type_ = 'resource'
-    if taxonomy is not None:
-        repo_instance = repo()
-        available_resources = getattr(repo_instance, method)()
-
-        missing_resource = validate_resource(available_resources, taxonomy,
-                                             type_)
-        if missing_resource:
-            error_response, error_code = missing_resource
-
-        matching_ids_ = [id_ for id_ in matching_ids if
-                         repo_instance.exists(id_, taxonomy)]
-        matching_ids = matching_ids_
-
-    repo = AlphaRepo
-    method = 'available_metrics'
-    type_ = 'metric'
-    if alpha_metric is not None:
-        repo_instance = repo()
-
-        # figure out if the user asked for a metric we have data on
-        available_metrics = getattr(repo_instance, method)()
-        missing_metric = validate_resource(available_metrics, alpha_metric,
-                                           type_)
-        if missing_metric:
-            error_response, error_code = missing_metric
-
-        # make sure all of the data the samples the user asked for have values
-        # for the given metric
-        matching_ids_ = [id_ for id_ in matching_ids if
-                         repo_instance.exists(id_, alpha_metric)]
-        matching_ids = matching_ids_
+    matching_ids, error_code, error_response = _filter_matching_ids(
+        matching_ids, AlphaRepo, 'available_metrics', alpha_metric,
+        'metric', error_response=error_response, error_code=error_code,
+    )
 
     if error_response:
         return jsonify(*error_response), error_code
 
     return jsonify(sample_ids=matching_ids), 200
+
+
+def _filter_matching_ids(matching_ids, repo, category, value, resource_type,
+                         error_response=None, error_code=None):
+    if value is not None:
+        repo_instance = repo()
+        available_resources = getattr(repo_instance, category)()
+
+        missing_resource = validate_resource(available_resources, value,
+                                             resource_type)
+        if missing_resource:
+            error_response, error_code = missing_resource
+
+        else:
+            matching_ids_ = [id_ for id_ in matching_ids if
+                             repo_instance.exists(id_, value)]
+            matching_ids = matching_ids_
+    return matching_ids, error_code, error_response
 
 
 def _validate_query(dict_, repo):
