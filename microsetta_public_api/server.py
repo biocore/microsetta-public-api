2c7313d7e5155984a4f05f493e17ac78ea2ff58c
George Armstrong <garmstrong@colgate.edu>
2020-07-23 14:00:45 -0700

    Update microsetta_public_api/server.py port

--- microsetta_public_api/server.py
+++ microsetta_public_api/server.py
@@ -29,12 +29,13 @@ def build_app(resources_config_json=None):
 if __name__ == "__main__":
     import sys
     config_fp = sys.argv[1] if len(sys.argv) > 1 else None
+    PORT = 8084
     if config_fp:
         app = build_app(resources_config_json=config_fp)
-        app.run(port=8083, debug=True)
+        app.run(port=PORT, debug=True)
     else:
         # import TestDatabase here to avoid circular import
         from microsetta_public_api.utils.testing import TestDatabase
         with TestDatabase():
             app = build_app()
-            app.run(port=8083, debug=True)
+            app.run(port=PORT, debug=True)
