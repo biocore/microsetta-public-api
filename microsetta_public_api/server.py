a1e709d36e90f70ff99ce093b455dbcc9d6e0c94
Daniel McDonald <danielmcdonald@ucsd.edu>
2020-07-24 13:35:16 -0700

    More closely mirror config handling for other microsetta- projects

--- microsetta_public_api/server.py
+++ microsetta_public_api/server.py
@@ -1,6 +1,7 @@
 import json
 from pkg_resources import resource_filename
-from microsetta_public_api import config
+from microsetta_public_api.config import (SERVER_CONFIG,
+                                          resources as config_resources)
 from microsetta_public_api.resources import resources
 
 import connexion
@@ -14,9 +15,9 @@ def build_app(resource_updates=None):
     # microsetta.config.resources, this config can be updated by a json file
     # passed to `build_app`.
     if resource_updates is not None:
-        config.resources.update(resource_updates)
+        config_resources.update(resource_updates)
 
-        resources.update(config.resources)
+        resources.update(config_resources)
 
     app_file = resource_filename('microsetta_public_api.api',
                                  'microsetta_public_api.yml')
@@ -28,24 +29,17 @@ def build_app(resource_updates=None):
 
 
 if __name__ == "__main__":
-    import sys
-    config_fp = sys.argv[1] if len(sys.argv) > 1 else None
+    resource_config = SERVER_CONFIG.get('resources', {})
+    port = SERVER_CONFIG['port']
+    debug = SERVER_CONFIG['debug']
+    use_test_database = SERVER_CONFIG['use_test_database']
 
-    if config_fp is None:
-        resource_config = None
-        server_config = {}
-    else:
-        with open(config_fp) as fp:
-            server_config = json.load(fp)
-        resource_config = server_config.get('resources', None)
-
-    port = server_config.get('port', 8084)
-    if config_fp:
-        app = build_app(resource_updates=resource_config)
-        app.run(port=port, debug=True)
-    else:
+    if use_test_database:
         # import TestDatabase here to avoid circular import
         from microsetta_public_api.utils.testing import TestDatabase
         with TestDatabase():
             app = build_app()
             app.run(port=port, debug=True)
+    else:
+        app = build_app(resource_updates=resource_config)
+        app.run(port=port, debug=debug)
