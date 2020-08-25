ea22606c4f7ca393370289313b370716cc13fd36
gwarmstrong <garmstrong@colgate.edu>
2020-08-25 12:13:06 -0700

    FEAT alpha and pcoa repo changes for multiple datasets

--- microsetta_public_api/repo/_alpha_repo.py
+++ microsetta_public_api/repo/_alpha_repo.py
@@ -1,4 +1,5 @@
-from microsetta_public_api.exceptions import UnknownMetric
+import pandas as pd
+from microsetta_public_api.exceptions import UnknownMetric, UnknownID
 from microsetta_public_api.resources import resources as RESOURCES
 
 
@@ -53,16 +54,26 @@ class AlphaRepo:
             union of samples ids in the database the ids in `sample_ids`.
             Sets the name of the series to `metric`.
 
+        Raises
+        ------
+
+        UnknownMetric
+            If the metric is not in the repo's resources
+        Unknown Id
+            If the id does not have value for the requested metric
+
         """
         # this could raise an UnknownMetric or ConfigurationError
         alpha_series = self._get_resource(metric)
-        # TODO the following could throw KeyErrors if a sample id is not in
-        #  the index. Right now, the API checks for missing ID's before
-        #  calling this.
         if isinstance(sample_ids, str):
-            return alpha_series.loc[[sample_ids]]
+            ids = pd.Series([sample_ids])
         else:
-            return alpha_series.loc[sample_ids]
+            ids = pd.Series(sample_ids)
+        unknown = ~ids.isin(alpha_series.index)
+        if any(unknown):
+            raise UnknownID(f"For metric='{metric}', unknown ids: "
+                            f"{ids.loc[unknown]}")
+        return alpha_series.loc[ids]
 
     def exists(self, sample_ids, metric):
         """Checks if sample_ids exist for the given metric.
