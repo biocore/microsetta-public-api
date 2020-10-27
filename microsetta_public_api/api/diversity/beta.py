bfce19ac13db2c4269c98d6cfa3209d39f7be327
gwarmstrong <garmstrong@colgate.edu>
2020-10-26 17:58:48 -0700

    NEW implementation for beta k nearest endpoint

--- microsetta_public_api/api/diversity/beta.py
+++ microsetta_public_api/api/diversity/beta.py
@@ -1,3 +1,25 @@
+from microsetta_public_api.utils import jsonify
+from microsetta_public_api.resources_alt import get_resources
+from microsetta_public_api.repo._beta_repo import BetaRepo
+from microsetta_public_api.exceptions import (
+    UnknownResource,
+)
+from microsetta_public_api.config import schema
+
+
+def _validate_dataset_beta(dataset, resource_getter):
+    try:
+        dataset_resource = resource_getter().gets('datasets', dataset)
+    except KeyError:
+        raise UnknownResource(f"Unknown dataset: '{dataset}'")
+    try:
+        beta_resource = dataset_resource.gets(schema.beta_kw)
+    except KeyError:
+        raise UnknownResource(f"No beta data (kw: '{schema.beta_kw}') for "
+                              f"dataset='{dataset}'.")
+    return beta_resource
+
+
 def pcoa_contains_alt(named_sample_set, sample_id):
     raise NotImplementedError()
 
@@ -6,5 +28,12 @@ def pcoa_contains(named_sample_set, sample_id):
     raise NotImplementedError()
 
 
-def k_nearest(dataset, beta_metric, k):
-    raise NotImplementedError()
+def k_nearest(dataset, beta_metric, k, sample_id):
+    beta_resource = _validate_dataset_beta(dataset,
+                                           resource_getter=get_resources)
+    beta_repo = BetaRepo(beta_resource)
+    k_nearest_ids = beta_repo.k_nearest(sample_id=sample_id,
+                                        metric=beta_metric,
+                                        k=k,
+                                        )
+    return jsonify(k_nearest_ids), 200
