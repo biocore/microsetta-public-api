b1cf77a6e11e251a40bd486c499a33b3c30f561f
gwarmstrong <garmstrong@colgate.edu>
2021-01-21 14:03:05 -0800

    FIX formatting of pid

--- microsetta_public_api/logging.py
+++ microsetta_public_api/logging.py
@@ -1,9 +1,11 @@
 import logging
 from functools import wraps
 from time import time
+import os
 
+FORMAT = '%(asctime)s PID={pid} %(levelname)s: %(message)s '
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s: %(message)s ')
+                    format=FORMAT.format(pid=os.getpid()))
 logger = logging.getLogger(__name__)
 
 
@@ -16,13 +18,14 @@ class timeit:
         @wraps(f)
         def wrapper(*args, **kwargs):
             logger.info('%(message)s Start.',
-                        {'message': self.msg}
+                        {'message': self.msg},
                         )
             start = time()
             result = f(*args, **kwargs)
             end = time()
             total = end - start
             logger.info('%(message)s Elapsed: %(elapsed)s',
-                        {'message': self.msg, 'elapsed': total})
+                        {'message': self.msg, 'elapsed': total},
+                        )
             return result
         return wrapper
