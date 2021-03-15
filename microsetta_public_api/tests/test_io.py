f4deadde96bda421b4eef2c6ba61155f0af8666f
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-03-14 18:34:00 -0700

    revert neighbors caching on load

--- microsetta_public_api/tests/test_io.py
+++ microsetta_public_api/tests/test_io.py
@@ -1,10 +1,7 @@
 from qiime2 import Artifact
 from skbio import DistanceMatrix
-import pandas as pd
-import pandas.testing as pdt
 from microsetta_public_api.utils.testing import TempfileTestCase
-from microsetta_public_api._io import (_dict_of_paths_to_beta_data,
-                                       _closest_k_from_distance_matrix)
+from microsetta_public_api._io import _dict_of_paths_to_beta_data
 
 
 class TestResourceIO(TempfileTestCase):
@@ -12,20 +9,12 @@ class TestResourceIO(TempfileTestCase):
     def test_load_beta_data(self):
         self.dm = DistanceMatrix(
             [
-                [0, 2, 1, 4],
-                [2, 0, 3, 5],
-                [1, 3, 0, 4.1],
-                [4, 5, 4.1, 0]
-            ], ids=['s1', 's2', 's3', 's4'],
+                [0, 1, 2],
+                [1, 0, 3],
+                [2, 3, 0],
+            ], ids=['s1', 's2', 's3'],
         )
 
-        exp = pd.DataFrame([['s3', 's2', 's4'],
-                            ['s1', 's3', 's4'],
-                            ['s1', 's2', 's4'],
-                            ['s1', 's3', 's2']],
-                           index=['s1', 's2', 's3', 's4'],
-                           columns=['k0', 'k1', 'k2'])
-        exp.index.name = 'sample_id'
         self.dm_fh = self.create_tempfile(suffix='.qza')
         self.dm_path = self.dm_fh.name
 
@@ -38,29 +27,8 @@ class TestResourceIO(TempfileTestCase):
 
         obs_dm = _dict_of_paths_to_beta_data({
             'beta1': self.dm_path,
-        }, '__beta__')['beta1']
+        }, '__beta__')
 
-        pdt.assert_frame_equal(obs_dm, exp)
-
-    def test_closest_k_from_distance_matrix(self):
-        dm = DistanceMatrix(
-            [
-                [0, 1, 2, 4, 5, 6],
-                [1, 0, 3, 4, 5, 6],
-                [2, 3, 0, 1, 1, 1],
-                [4, 4, 1, 0, 2, 1],
-                [5, 5, 1, 2, 0, 3],
-                [6, 6, 1, 1, 3, 0],
-            ], ids=['s1', 's2', 's3', 's4', 's5', 's6'],
-        )
-        exp = pd.DataFrame([['s2', 's3'],
-                            ['s1', 's3'],
-                            ['s4', 's5'],
-                            ['s3', 's6'],
-                            ['s3', 's4'],
-                            ['s3', 's4']],
-                           index=['s1', 's2', 's3', 's4', 's5', 's6'],
-                           columns=['k0', 'k1'])
-        exp.index.name = 'sample_id'
-        obs = _closest_k_from_distance_matrix(dm, 2)
-        pdt.assert_frame_equal(obs, exp)
+        self.assertDictEqual(
+            {'beta1': self.dm},
+            obs_dm)
